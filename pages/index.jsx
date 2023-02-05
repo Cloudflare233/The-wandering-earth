a9fbfa491521e178fdf25e1ceaba48b4a0b6a39e
Cloudflare233 <Cloudflare233@yandex.com>
2023-02-05 11:40:17 +0800

    一些小调整

--- pages/index.jsx
+++ pages/index.jsx
@@ -23,7 +23,7 @@ export default function Home() {
         <h1>流浪地球2：倒计时在线生成器</h1>
         <div className='flex flex-row space-x-2'>
           <p className='inter font-medium'>V0.2 Alpha</p>
-          <button onClick={()=>open('https://github.com/Cloudflare233/The-wandering-earth')}>
+          <button onClick={() => open('https://github.com/Cloudflare233/The-wandering-earth')}>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
               <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
             </svg>
@@ -36,18 +36,18 @@ export default function Home() {
       <div className='my-6 grid grid-cols-1 sm:grid-cols-3 gap-4'>
         <div className='node-content w-full col-span-2 overflow-x-auto bg-black text-white rounded-3xl min-h-[16rem] sm:min-h-[24rem] px-8 sm:px-36 py-24'>
           <div ref={domRef} className={cn(bg === 'false' ? '' : 'bg-black')}>
-            <h2 className='text-4xl mb-3'>距离{event}</h2>
+            <h2 className='text-4xl mb-3 word'>距离{event}</h2>
             <div className='grid grid-cols-3 grid-rows-2'>
               <div className='row-span-2 col-span-1 ml-12 word h-auto bg-red-600 w-1.5' />
               <div className='col-span-2 -ml-4 sm:-ml-7'>
-                <div className='flex flex-row'>
-                  <h2 className='text-4xl word'>还有</h2>
+                <div className='flex flex-row word flex-nowrap'>
+                  <h2 className='text-4xl'>还有</h2>
                   <h2 className='text-6xl text-red-600 -mt-3 font-bold ml-1 mr-1 inter'>{time}</h2>
-                  <h2 className='text-4xl word'>{unit}</h2>
+                  <h2 className='text-4xl'>{unit}</h2>
                 </div>
               </div>
               <div className='col-span-2 -ml-4 sm:-ml-7'>
-                <div className='row-span-1 inter mt-1 opacity-75 w-56'>
+                <div className='row-span-1 inter mt-0.5 opacity-75 w-56'>
                   {english}
                 </div>
               </div>
@@ -58,9 +58,17 @@ export default function Home() {
         </div>
 
         <div className='flex flex-col space-y-3 px-0 sm:px-1'>
-          <h2 className='opacity-75 text-sm'>在这里补充需要的信息</h2>
+          <div className='flex flex-row justify-between'>
+            <h2 className='opacity-75 text-sm'>在这里补充需要的信息</h2>
+            <button onClick={()=>{setEvent('');setTime('');setUnit('');setEnglish('')}} className='flex flex-row space-x-1 text-sm opacity-50'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+              </svg>
+              <span>清空</span>
+            </button>
+          </div>
 
-          <div className='flex flex-col space-y-2'>
+          <div className='flex flex-col space-y-2 opacity-90'>
             <input onChange={(e) => setEvent(e.target.value)} value={event} className='w-full rounded-lg px-4 py-2 bg-zinc-100 border border-zinc-200' placeholder='事件' />
             <input onChange={(e) => setTime(e.target.value)} value={time} className='inter w-full rounded-lg px-4 py-2 bg-zinc-100 border border-zinc-200' placeholder='时间（数字）' />
             <input onChange={(e) => setUnit(e.target.value)} value={unit} className='w-full rounded-lg px-4 py-2 bg-zinc-100 border border-zinc-200' placeholder='单位' />
